110cde3441cbad4b68d4611530491d4559bf0924
Quinten Aiton <QuinAiton@hotmail.com>
2020-09-18 15:19:37 -0700

    created a comment model and added comment functionality to listed items

---
+++ models/Item.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  description: String,
+  price: String,
+  created: { type: Date, default: Date.now },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    username: String,
+  },
+});
+
+module.exports = mongoose.model("Item", itemSchema);
---
+++ routes/comments.js
@@ -0,0 +1,32 @@
+const express = require("express"),
+  router = express.Router({ mergeParams: true }),
+  Item = require("../models/Item"),
+  Comment = require("../models/Comment"),
+  middleware = require("../middleware");
+
+//comments route
+router.get("/gallery/:id", (req, res) => {
+  Item.findById(req.params.id, (err, foundItem) =>
+    err ? console.log(err) : res.render("items/show", { item: foundItem })
+  );
+});
+
+router.post("/gallery/:id", (req, res) => {
+  Item.findById(req.params.id, (err, foundItem) => {
+    if (err) {
+      console.log(err);
+    } else {
+      Comment.create(req.body.comment, (err, comment) => {
+        if (err) {
+          console.log(err);
+        } else {
+          foundItem.comments.push(comment);
+          foundItem.save();
+          res.redirect("/gallery/" + foundItem._id);
+        }
+      });
+    }
+  });
+});
+
+module.exports = router;
